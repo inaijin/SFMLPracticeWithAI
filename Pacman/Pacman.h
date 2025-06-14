1a4e06e2657a9d961fb2d5328b9a0dc28df0aaa6
inaijin <kouroshsajjadi@yahoo.com>
2025-06-14 18:48:12 +0330

    add Pacman.h For Pacman

---
+++ Pacman/Pacman.h
@@ -0,0 +1,16 @@
+#ifndef PACMAN_H
+#define PACMAN_H
+
+#include "Tile.h"
+#include "GameObject.h"
+
+class Pacman : public GameObject {
+public:
+    Pacman(std::vector<float> dimensions, std::vector<float> initPosition);
+    void update(std::vector<std::vector<Tile>> map) override;
+
+private:
+    void handleInput();
+};
+
+#endif // PACMAN_H
