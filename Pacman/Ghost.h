8963d1bff870411925cb467155b11a4397ad126d
inaijin <kouroshsajjadi@yahoo.com>
2025-06-14 18:48:11 +0330

    add Ghost.h For Pacman

---
+++ Pacman/Ghost.h
@@ -0,0 +1,16 @@
+#ifndef GHOST_H
+#define GHOST_H
+
+#include "GameObject.h"
+
+class Ghost : public GameObject {
+public:
+    Ghost(std::vector<float> dimensions, std::vector<float> initPosition, sf::Color color);
+    void update(std::vector<std::vector<Tile>> map) override;
+    void randomMovement();
+
+private:
+
+};
+
+#endif // GHOST_H
