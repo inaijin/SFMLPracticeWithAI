13597c06c09f102456d587eb69f77581c7f1c108
inaijin <kouroshsajjadi@yahoo.com>
2025-06-14 18:48:11 +0330

    add GameObject.h For Pacman

---
+++ Pacman/GameObject.h
@@ -0,0 +1,28 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+#include <SFML/Graphics.hpp>
+
+#include "Tile.h"
+
+class GameObject {
+public:
+    GameObject(std::vector<float> dimensions, std::vector<float> initPosition, float speed);
+    void virtual update(std::vector<std::vector<Tile>> map);
+    void draw(sf::RenderWindow& window);
+    void setPosition(const sf::Vector2f& newPosition);
+    sf::Vector2f getNextPosition() const;
+    const sf::RectangleShape& getShape() const;
+
+    sf::Vector2f getLastDirection() const { return lastDirection; }
+    float getSpeed() { return speed; }
+
+protected:
+    sf::RectangleShape objectShape;
+    sf::Vector2f direction;
+    sf::Vector2f lastDirection;
+    float speed;
+    void handleColition(std::vector<std::vector<Tile>> map);
+};
+
+#endif // GAMEOBJECT_H
