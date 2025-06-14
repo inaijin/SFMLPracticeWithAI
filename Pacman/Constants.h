b43764c5a4d93fb05857600273197f3b1fa552e3
inaijin <kouroshsajjadi@yahoo.com>
2025-06-14 18:48:11 +0330

    add Constants.h For Pacman

---
+++ Pacman/Constants.h
@@ -0,0 +1,18 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <vector>
+
+#include "Tile.h"
+
+// Screen dimensions
+const int SCREEN_WIDTH = 1600;
+const int SCREEN_HEIGHT = 1200;
+const int framePS = 120;
+
+// Game Objets Diemnsions
+const std::vector<float> ghost_diemensions = {30, 30};
+const std::vector<float> pacman_diemensions = {30, 30};
+const std::vector<float> tile_diemensions = {30, 30};
+
+#endif // CONSTANTS_H
